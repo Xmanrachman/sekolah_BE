5af37e8280e6c4c28a7be840cb53c18b59241db3
xman <106719630+Xmanrachman@users.noreply.github.com>
2024-05-31 15:30:25 +0700

    FistCommit

---
+++ sekolah-parent/src/main/java/com/managemen/sekolah/sekolah_parent/SekolahParentApplication.java
@@ -0,0 +1,13 @@
+package com.managemen.sekolah.sekolah_parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SekolahParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SekolahParentApplication.class, args);
+	}
+
+}
---
+++ sekolah-parent/src/test/java/com/managemen/sekolah/sekolah_parent/SekolahParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.managemen.sekolah.sekolah_parent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SekolahParentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
